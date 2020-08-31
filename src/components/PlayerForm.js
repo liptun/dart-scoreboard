666d35e72bb702a0c337759d06c71d349847b8e0
Rafał Karczmarzyk <liptunred@gmail.com>
2020-08-31 11:10:52 +0200

    Handle remove player

--- src/components/PlayerForm.js
+++ src/components/PlayerForm.js
@@ -6,7 +6,7 @@ import '../styles/playerForm.scss'
 const PlayerForm = (props) => {
     const [playerName, setPlayerName] = useState('')
 
-    const submitForm = (e) => {
+    const onFormSubmit = (e) => {
         e.preventDefault()
         if ( playerName.trim() ) {
             props.dispatch(addPlayer({name: playerName}))
@@ -14,19 +14,24 @@ const PlayerForm = (props) => {
         }
     }
 
+    const onPlayerRemove = (id) => {
+        props.dispatch(removePlayer({id}))
+    }
+
     return (
         <div className="player-form">
             <h2>Player list</h2>
             <div className="player-list">
+                {props.playerList.length === 0 && <p>Add new player</p>}
                 {props.playerList.map( player => (
                     <div className="player-item" key={player.id}>
                         <p>{player.name}</p>
-                        <button>&times;</button>
+                        <button onClick={() => onPlayerRemove(player.id)}>&times;</button>
                     </div>
                 ) )}
             </div>
             <div className="player-add">
-                <form onSubmit={submitForm}>
+                <form onSubmit={onFormSubmit}>
                     <input type="text" placeholder="Player name" value={playerName} onChange={(e) => setPlayerName(e.target.value)} />
                     <button>Add</button>
                 </form>
