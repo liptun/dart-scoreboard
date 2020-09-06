8ec2ce68cc2f6735d82e1fff26a36fa68119b78c
LiptuN <liptunred@gmail.com>
2020-09-06 21:55:21 +0200

    Footer

--- src/Router.js
+++ src/Router.js
@@ -1,19 +1,21 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import NotFoundPage from './components/NotFoundPage'
 import NewGamePage from './components/NewGamePage'
 import GamePage from './components/GamePage'
+import Footer from './components/Footer'
 
 const Router = () => (
     <BrowserRouter>
-        <React.Fragment>
+        <Fragment>
             <Switch>
                 <Route path="/" component={NewGamePage} exact={true} />
                 <Route path="/game" component={GamePage} exact={true} />
                 <Route path="/setup" component={NewGamePage} />
                 <Route component={NotFoundPage} />
             </Switch>
-        </React.Fragment>
+            <Footer />
+        </Fragment>
     </BrowserRouter>
 )
 export default Router
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import '../styles/footer.scss'
+
+const Footer = () => (
+    <footer className="footer">
+        <p>
+            Developed by{' '}
+            <a href="https://github.com/liptun" target="_blank">
+                LiptuN
+            </a>
+        </p>
+    </footer>
+)
+export default Footer
