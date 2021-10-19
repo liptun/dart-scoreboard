e30e662f117161ac2d02b5fd30f738ae8aec5f32
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2021-10-19 13:04:00 +0200

    cleanup

--- src/tests/components/Footer.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import Footer from '../../components/Footer'
-
-test('Render Footer component', () => {
-    const wrapper = shallow(<Footer />)
-    expect(wrapper).toMatchSnapshot()
-})
